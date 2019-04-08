c3a5d43df3b91e4c554ffb2394f2f48353bd1702
Angi <wangs817@uw.edu>
2019-04-07 22:27:48 -0700

    Update doubleList.java

--- chapter10/doubleList.java
+++ chapter10/doubleList.java
@@ -1,16 +1,8 @@
 /* Write a method doubleList that takes an ArrayList of Strings as a parameter 
  * and that replaces every string with two of that string.
  */
-public void doubleList(ArrayList<String> arr) {
-    ArrayList<String> temp = new ArrayList<String>();
-    
-    for(String str : arr) {
-        temp.add(str);
-        temp.add(str);
-    }
-    
-    arr.clear();
-    
-    for(String str : temp)
-        arr.add(str);
-}
+   public static void doubleList(ArrayList<String> list) {
+      for (int i = 0; i < list.size(); i += 2) {
+         list.add(i+1,list.get(i));
+      }
+   }
